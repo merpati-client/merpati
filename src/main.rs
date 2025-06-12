fda5f26c89a5aceaea99969cda1567e69ccda02b
secona <secona00@gmail.com>
2025-06-12 15:28:42 +0700

    feat: add button to app

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
-use iced::Element;
-use iced::widget::text_input;
+use iced::widget::{button, row, text_input, Row};
 
 fn main() -> iced::Result {
-    iced::application("Merpati", update, view)
+    iced::application("Merpati", Http::update, Http::view)
         .theme(|_| iced::Theme::CatppuccinMocha)
         .run()
 }
 
 #[derive(Default)]
-struct State {
+struct Http {
    content: String,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    ContentChanged(String)
+    ContentChanged(String),
+    SendRequest,
 }
 
-fn view(state: &State) -> Element<'_, Message> {
-    text_input("Type something here...", &state.content)
-        .on_input(Message::ContentChanged)
-        .into()
-}
+impl Http {
+    fn view(&self) -> Row<'_, Message> {
+        row![
+            text_input("URL", &self.content)
+                .on_input(Message::ContentChanged),
+            button("Send").on_press(Message::SendRequest),
+        ]
+    }
 
-fn update(state: &mut State, message: Message) {
-    match message {
-        Message::ContentChanged(content) => {
-            state.content = content;
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ContentChanged(content) => self.content = content,
+            Message::SendRequest => println!("Sending request to {}", self.content),
         }
     }
 }
