5393a19d8c4dfadfa0c6d7267042fcecb5448c19
secona <secona00@gmail.com>
2025-07-03 13:36:08 +0700

    chore(nix): update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,50 +3,32 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    git-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-
-    naersk.url = "github:nix-community/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    inputs@{
-      nixpkgs,
-      flake-parts,
-      rust-overlay,
-      naersk,
-      ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      perSystem = (
-        {
-          pkgs,
-          system,
-          ...
-        }:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ (import rust-overlay) ];
-          };
 
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        let
           toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-          naersk' = pkgs.callPackage naersk {
-            cargo = toolchain;
-            rustc = toolchain;
-            clippy = toolchain;
-          };
-
-          merpati = naersk'.buildPackage {
-            name = "merpati";
-            version = "0.1.0";
-            src = ./.;
-          };
-
-          buildInputs = with pkgs; [
+          icedBuildInputs = with pkgs; [
             expat
             fontconfig
             freetype
@@ -62,16 +44,34 @@
           ];
         in
         {
-          formatter = pkgs.nixfmt-rfc-style;
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.rust-overlay.overlays.default ];
+          };
 
-          packages.default = merpati;
+          treefmt.programs = {
+            nixfmt.enable = true;
+            rustfmt.enable = true;
+            rustfmt.package = toolchain;
+          };
+
+          pre-commit.settings.hooks = {
+            clippy.enable = true;
+            clippy.packageOverrides.cargo = toolchain;
+            clippy.packageOverrides.clippy = toolchain;
+          };
 
           devShells.default = pkgs.mkShell {
             name = "merpati";
-            buildInputs = buildInputs ++ [ toolchain pkgs.openssl pkgs.pkg-config ];
-            LD_LIBRARY_PATH = builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" buildInputs;
+            buildInputs = icedBuildInputs ++ [
+              toolchain
+              pkgs.openssl
+              pkgs.pkg-config
+            ];
+            LD_LIBRARY_PATH = builtins.foldl' (
+              a: b: "${a}:${b}/lib"
+            ) "${pkgs.vulkan-loader}/lib" icedBuildInputs;
           };
-        }
-      );
+        };
     };
 }
